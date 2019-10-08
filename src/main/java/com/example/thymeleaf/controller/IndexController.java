12fa8870422007e11c9da124e2e53a24b23f70bc
Sebastien Ots <ots.sebastien@gmail.com>
2019-10-08 12:13:04 +0200

    Spring03

--- src/main/java/com/example/thymeleaf/controller/IndexController.java
+++ src/main/java/com/example/thymeleaf/controller/IndexController.java
@@ -3,7 +3,6 @@ package com.example.thymeleaf.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
 public class IndexController {
 
     @GetMapping("/")
